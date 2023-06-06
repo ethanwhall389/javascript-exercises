4849ab3bb9084cd5081f52d23be845cac38b7234
Ethan Hall <ethanwhall@icloud.com>
2023-06-05 21:41:39 -0400

    Complete 1-3, complete first test 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split('');
+    array.reverse();
+    let reversedString = array.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, numToRemove) {
+    let newArray = [];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] != numToRemove) {
+            newArray.push(array[i]);
+        } else {
+            continue;
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
