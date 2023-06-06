59d36f83820e02e344a505574fea3273a1161c0f
Ethan Hall <ethanwhall@icloud.com>
2023-06-06 21:41:24 +0000

    Work on 08; complete add and subtract

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,9 +1,17 @@
 const add = function() {
-	
+	let totalSum = 0;
+  for (i = 0; i < arguments.length; i++) {
+    totalSum+= arguments[i];
+  }
+  return totalSum;
 };
 
 const subtract = function() {
-	
+	let finalSubtraction = arguments[0];
+  for (i = 1; i < arguments.length; i++) {
+    finalSubtraction-= arguments[i];
+  }
+  return finalSubtraction;
 };
 
 const sum = function() {
