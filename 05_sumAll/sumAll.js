92987881fa87a746444caf862b625808061d01bc
Ethan Hall <ethanwhall@icloud.com>
2023-06-06 21:11:50 +0000

    Complete leapYears.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,25 @@
 const sumAll = function(startNum, endNum) {
     let finalSum = 0;
-    if (startNum < 0 || endNum < 0) {
-        return'ERROR';
-    }
-    else if (startNum < endNum) {
-       for (i = startNum; i < endNum + 1; i++) {
-        finalSum += i;
-       }
-        return finalSum
-    } else if (startNum > endNum) {
+    if (typeof startNum != 'number' || typeof endNum != 'number') {
+        return 'ERROR';
+    } else {
+
+        if (startNum < 0 || endNum < 0) {
+            return'ERROR';
+        }
+        else if (startNum < endNum) {
+            for (i = startNum; i < endNum + 1; i++) {
+            finalSum += i;
+        }
+            return finalSum
+        } else if (startNum > endNum) {
         for (i = endNum; i < startNum + 1; i++) {
             finalSum += i;
         }
         return finalSum
-    } 
+        } 
+
+    }
     
     
 };
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(enteredYear) {
+    if (enteredYear % 4 === 0 && enteredYear % 100 != 0) {
+        return true;
+    }
+    else if (enteredYear % 100 === 0 && enteredYear % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+    };
 
-};
 
 // Do not edit below this line
 module.exports = leapYears;
