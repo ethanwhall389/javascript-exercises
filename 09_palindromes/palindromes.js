194aa8069ce977d2d6c566c183e04af473fde687
Ethan Hall <ethanwhall@icloud.com>
2023-06-07 19:42:23 -0400

    Work on 09palin- stuck on return

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+// remove all of the spaces from the string
+// create a new variable with the string reversed
+// compare the new variable with the old
+
+
+
+const palindromes = function (string) {
+    // splits string into array and removes the spaces.
+    let original = string.split(' ');
+    let reversed = original.reverse();
+    if (original === reversed) {
+        return true;
+    } else {
+        return false;
+    }
+
 
 };
 
