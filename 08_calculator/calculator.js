eeeb3852778e5284d342b6091ece3319f1a967e3
Ethan Hall <ethanwhall@icloud.com>
2023-06-07 19:26:59 -0400

    Complete Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -22,16 +22,32 @@ const sum = function(array) {
   return Number(sum);
 }; 
 
-const multiply = function() {
-
+const multiply = function(array) {
+  // start product at first num, since 0 * anything = 0.
+  let product = array[0];
+  let i = 1;
+  while (i < array.length) {
+    product = product * array[i];
+    i++;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = num;
+  if (num === 0) {
+    factorial = 1;
+  } else {
+    for (let i = num - 1; i > 0; i--) {
+    factorial = factorial * i;
+    } 
+  }
+  return factorial;
+  
 };
 
 // Do not edit below this line
