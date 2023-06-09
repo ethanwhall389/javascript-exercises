653b1fe648f8bff2494cca65c6911dfdf53ad9e1
Ethan Hall <ethanwhall@icloud.com>
2023-06-09 19:56:31 +0000

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+//using .reduce()
+//
 
+const findTheOldest = function(array) {
+  return array.reduce(function(accumulator, currentValue) {
+    let accumulatorAge = getAge(accumulator.yearOfBirth, accumulator.yearOfDeath);
+    let currentValueAge = getAge(currentValue.yearOfBirth, currentValue.yearOfDeath);
+    let finalName;
+    if (accumulatorAge > currentValueAge) {
+        return accumulator;
+    } else {
+        return currentValue;
+    }
+
+   })
+    
 };
+
+const getAge = function(birth, death) {
+    if (death === undefined) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
